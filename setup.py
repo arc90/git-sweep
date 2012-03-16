a0441e7b0de3688d023f303bdeef05849ad290ef
robmadole <robmadole@gmail.com>
2012-03-16 18:52:44 -0500

    Fixing version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.1'
+version = '0.1.0'
 
 install_requires = [
     'GitPython>=0.3.2RC1']
